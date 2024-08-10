fc5adde416e13858917ff92c1a07dab2c732858d
gokul-saaragh <gokulchandan24@gmail.com>
2024-08-10 18:05:10 +0530

    built app

--- app/app/admin/layout.jsx
+++ app/app/admin/layout.jsx
@@ -1,10 +1,23 @@
 import Sidebar from "@/components/AdminComponents/Sidebar";
+import Image from "next/image";
+import profile_icon from "../../assets/profile_icon.png";
 
 export default function Layout({ children }) {
   return (
     <>
       <div className="flex">
         <Sidebar />
+        <div className="flex flex-col w-full">
+          <div className="flex items-center justify-between w-full py-3 mx-h-[60px] px-12 border-b border-black">
+            <h3 className="font-medium">Admin Panel</h3>
+            <Image
+              width={40}
+              src={profile_icon}
+              alt=""
+              className="w-[130px] rounded sm:w-auto"
+            />
+          </div>
+        </div>
       </div>
       {children}
     </>
--- app/components/AdminComponents/Sidebar.jsx
+++ app/components/AdminComponents/Sidebar.jsx
@@ -1,28 +1,60 @@
 import React from "react";
 import blog_logo from "../../assets/blog_logo.webp";
 import plus_icon from "../../assets/plus_icon.png";
+import list_icon from "../../assets/list_icon.png";
+import sub_icon from "../../assets/sub_icon.png";
 import Image from "next/image";
+import Link from "next/link";
 
 const Sidebar = () => {
   return (
     <div className="flex flex-col bg-slate-200">
       <div className="px-2 sm:pl-14 py-3 border border-black">
         <Image
-          width={100}
+          width={95}
           src={blog_logo}
           alt=""
           className="w-[130px] rounded sm:w-auto"
         />
       </div>
       <div className="w-28 sm:w-80 h-[100vh] relative py-12 border border-blacl">
-        <div className="flex items-center cursor-pointer border border-black gap-3 font-medium px-3 py-3 shadow-[-7px_7px_0px_#3d3d3d] ">
-          <Image
-            width={28}
-            src={plus_icon}
-            alt=""
-            className="w-[130px] rounded sm:w-auto"
-          />
-          <p>Add Blogs</p>
+        <div className="w-[50%] sm:w-[80%] absolute right-0">
+          <Link
+            href="/admin/addProduct"
+            className="flex items-center cursor-pointer border border-black gap-3 font-medium px-3 py-3 shadow-[-7px_7px_0px_#3d3d3d] "
+          >
+            <Image
+              width={28}
+              src={plus_icon}
+              alt=""
+              className="w-[130px] rounded sm:w-auto"
+            />
+            <p>Add Blogs</p>
+          </Link>
+          <Link
+            href="/admin/blogList"
+            className="flex items-center my-5 cursor-pointer border border-black gap-3 font-medium px-3 py-3 shadow-[-7px_7px_0px_#3d3d3d] "
+          >
+            <Image
+              width={28}
+              src={list_icon}
+              alt=""
+              className="w-[130px] rounded sm:w-auto"
+            />
+            <p>Blog lists</p>
+          </Link>
+          <Link
+            href="/admin/subscriptions"
+            className="flex items-center cursor-pointer border border-black gap-3 font-medium px-3 py-3 shadow-[-7px_7px_0px_#3d3d3d] "
+          >
+            <Image
+              width={28}
+              src={sub_icon}
+              alt=""
+              className="w-[130px] rounded sm:w-auto"
+            />
+            <p>Subscriptions</p>
+          </Link>
         </div>
       </div>
     </div>
